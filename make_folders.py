205c56e87f7e29c2e0ddae01491d452338029931
buccalon <buccalon@users.noreply.github.com>
2020-04-08 04:00:40 -0300

    working version for validation

--- make_folders.py
+++ make_folders.py
@@ -1,52 +1,106 @@
-import os
-import openpyxl
+import os, csv, glob
 from PIL import Image, ImageDraw, ImageFont
 
 
-def load_data(path):
+def load_data(input_path, output_path):
     """
-    Read spreadsheet from file
+    Read data from csv file
     """
-    wb = openpyxl.load_workbook(path)
+    print("-" * 80)
+    print("LOADING DATA")
+    print("-" * 80)
 
-    sheet = wb["Plan1"]
+    try:
+        for filename in glob.glob(os.path.join(input_path, "*.csv")):
 
-    return sheet
+            print(filename)
+
+            # CREATE OUTPUT DIRECTORY FOR EACH FILE
+
+            output_folder_name = os.path.basename(filename).split(".")[0]
+            output_folder_path = os.path.join(output_path, output_folder_name)
+
+            if os.path.isdir(output_folder_path):
+                print(f"[*] Já existe uma pasta no diretório de saída. \n")
+            else:
+                os.mkdir(output_folder_path)
+
+            # OPEN FILE AND READ EACH ROW
+
+            with open(
+                os.path.join(os.getcwd(), filename), "r", encoding="utf-8"
+            ) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_counter = 0
+                draw_counter = 0
+                for row in csv_reader:
+                    if line_counter == 0:
+                        header = row
+                        line_counter += 1
+                    else:
+                        # DRAW FOLDER
+                        tags = dict(zip(header, row))
+                        draw_folders(output_folder_path, **tags)
+                        draw_counter += 1
+                        line_counter += 1
+
+                print(
+                    f"\n Foram criadas {draw_counter} capas a partir do arquivo {filename}"
+                )
+                print("-" * 80)
+
+    except Exception as e:
+        return print(e)
 
 
-def draw_folders(data):
+def draw_folders(path, **data):
     """
     Draw text in A3 page
     """
 
-    counter = 0
+    print(
+        f"[desenhando]  {data['record_name']}  {data['folder_number']}  {data['title'][:40]}..."
+    )
 
-    for rowOfCellObjects in data:
-        counter += 1
-        print(rowOfCellObjects)
-        im = Image.new("RGBA", (1600, 1200), "white")
-        draw = ImageDraw.Draw(im)
-        arialFont = ImageFont.truetype("arial.ttf", 22)
-        draw.text((0, 0), str(rowOfCellObjects[0].value), fill="black", font=arialFont)
-        draw.text((0, 40), str(rowOfCellObjects[1].value), fill="black", font=arialFont)
-        draw.text((0, 20), str(rowOfCellObjects[2].value), fill="black", font=arialFont)
-        draw.text((0, 60), "Col. 036 Diários Associados", fill="gray", font=arialFont)
-        im.save(
-            "images/"
-            + str(rowOfCellObjects[1].value)
-            + "-"
-            + str(rowOfCellObjects[0].value)
-            + ".png"
-        )
-    return print(f"Você gerou {counter} pastas")
+    im = Image.new("RGB", (842, 1191), "white")
+    draw = ImageDraw.Draw(im)
+
+    arialLarge = ImageFont.truetype("arial.ttf", 20)
+
+    arialSmall = ImageFont.truetype("arial.ttf", 14)
+
+    draw.text((60, 80), str(data["collection"]), fill="gray", font=arialSmall)
+
+    draw.text((60, 120), str(data["record_name"]), fill="black", font=arialLarge)
+
+    draw.text(
+        (700, 120), str(data["folder_number"]), fill="black", font=arialLarge,
+    )
+
+    draw.text((60, 180), str(data["title"]), fill="black", font=arialSmall)
+
+    draw.text(
+        (60, 230),
+        str("Número de fotografias: " + data["images_number"]),
+        fill="black",
+        font=arialSmall,
+    )
+
+    im.save(
+        path
+        + "/"
+        + str(data["record_name"])
+        + " ("
+        + str(data["folder_number"])
+        + ").pdf",
+        "PDF",
+        quality=100,
+    )
 
 
 def main():
 
-    PATH = "file.xlsx"
-
-    sheet = load_data(PATH)
-    draw_folders(sheet)
+    load_data("input", "output")
 
 
 if __name__ == "__main__":
